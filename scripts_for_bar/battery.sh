c3e83c2c3f3954a3c7b0078dfa037c8ec22e8efb
ViliamKovac1223 <viliamkovac1223@gmail.com>
2022-06-27 13:33:04 +0200

    add variable for battery folder

--- scripts_for_bar/battery.sh
+++ scripts_for_bar/battery.sh
@@ -3,10 +3,12 @@
 # $1 - first argument is time to show notification 
 # $2 - second argument define the battery critical level. If the battery is under battery critical level notify user
 
+BATERY="BAT0"
+
 [ -z $2 ] && battery_critical_level=15|| battery_critical_level=$2
 
-battery_capacity=$(cat "/sys/class/power_supply/BAT1/capacity") # get battery capacity in percentage
-battery_capacity_status=$(cat "/sys/class/power_supply/BAT1/status") # get battery status
+battery_capacity=$(cat "/sys/class/power_supply/$BATERY/capacity") # get battery capacity in percentage
+battery_capacity_status=$(cat "/sys/class/power_supply/$BATERY/status") # get battery status
   
 # if battery is under 15% and battery is not charging notify user about it
 [ "$battery_capacity" -lt $battery_critical_level ] && [ $battery_capacity_status != "Charging" ] && ( [ -z $1 ] \
